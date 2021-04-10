44553ba07fd042a528273d7bbfc0c01939d8fd37
nfeliberti <nf8091@gmail.com>
2021-04-10 17:17:53 -0400

    startofApi

---
+++ models/api.js
@@ -0,0 +1,77 @@
+const APIController = (function(){
+    const clientId = '';
+    const clientSecret='';
+
+    // private methods
+    const _getToken = async() => {
+        const result = await fetch('https://accounts.spotify.com/api/token',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlcoded',
+                'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+        const data = await result.json();
+        return data.access_token;
+        
+    }
+
+    const _getGenres = async (token) => {
+        
+        const result = await fetch (`https://api.spotify.com/v1/browse/categories?locale=sv_US`,{
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer' +token}
+        });
+
+        const data = await result.json();
+        return data.catergories.items;
+    }
+
+    const _getPlaylistByGenre = async (token, genreId) =>{
+
+        const limit = 10;
+
+        const result  = await fetch(`${tracksEndPoint}?limit=${limit}`,{
+            method: 'GET',
+            headers: {'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.playlists.items;
+    }
+
+    const _getTracks = async (token, tracksEndPoint) => {
+        const limit = 10;
+
+        const result = await fetch (`${tracksEndPoint}?limit=${limit}`,{
+            method: 'GET',
+            headers : {'Authorization' : 'Bearer' + token}
+        });
+
+        const data = result.json();
+        return data.items;
+    }
+
+    const _getTrack = async (token, trackEndPoint) =>{
+        const result = await fetch(`${trackEndPoint}`,{
+            method: 'GET',
+            headers: {'Authorization': 'Bearer' + token}
+        });
+
+        const data = await result.json();
+        return data;
+    }
+    
+    return{
+        getToken(){
+            return _getToken();
+        },
+        getgenres(token) {
+            return _getGenres(token);
+        },
+        getPlaylistByGenre(token, genreId);
+    },
+    getTracks(token, tracksEndPoint)
+
+})();
